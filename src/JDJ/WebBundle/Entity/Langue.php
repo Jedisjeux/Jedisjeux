d94118d5196721e31c2d28b1841a8e920a51d52a
Loic <lc.fremont@gmail.com>
2014-08-31 23:44:11 +0200

    [i] adding langue entity in web bundle

---
+++ src/JDJ/WebBundle/Entity/Langue.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace JDJ\WebBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Langue
+ */
+class Langue
+{
+    /**
+     * @var integer
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $libelle;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set libelle
+     *
+     * @param string $libelle
+     * @return Langue
+     */
+    public function setLibelle($libelle)
+    {
+        $this->libelle = $libelle;
+
+        return $this;
+    }
+
+    /**
+     * Get libelle
+     *
+     * @return string 
+     */
+    public function getLibelle()
+    {
+        return $this->libelle;
+    }
+}
