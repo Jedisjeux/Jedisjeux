1e73e6059f60b87324d3b19a46ef2fbbf202c701
Loic <lc.fremont@gmail.com>
2015-03-11 00:11:01 +0100

    rename nav tabs on game sheet user reviews

--- web/js/core/tab.js
+++ web/js/core/tab.js
@@ -1,7 +1,12 @@
 $(function() {
-    var hash = window.location.hash;
+    // Javascript to enable link to tab
+    var url = document.location.toString();
+    if (url.match('#')) {
+        $('.nav-tabs a[href=#'+url.split('#')[1]+']').tab('show') ;
+    }
 
-    // do some validation on the hash here
-
-    hash && $('ul.nav a[href="' + hash + '"]').tab('show');
+    // Change hash for page-reload
+    $('.nav-tabs a').on('shown', function (e) {
+        window.location.hash = e.target.hash;
+    })
 });
