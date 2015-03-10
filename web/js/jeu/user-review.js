52baea97e62d19ff3c90d1bedfc0945dc1b88ef1
Loic <lc.fremont@gmail.com>
2015-03-10 23:22:47 +0100

    user-review useless or not ?

--- web/js/jeu/user-review.js
+++ web/js/jeu/user-review.js
@@ -3,7 +3,7 @@ $(function() {
     if ($('#user-review').length > 0) {
         $('html, body').animate({
             scrollTop:$("#user-review").offset().top
-        }, 'slow');
+        }, 'fast');
     }
 
 });
