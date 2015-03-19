60590856156a8aa08f26fde7b4d336d11a9e0bb8
Loic <lc.fremont@gmail.com>
2015-03-19 23:33:59 +0100

    behat doc and behat utils context

---
+++ src/JDJ/CoreBundle/Behat/UtilsContext.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: loic_425
+ * Date: 25/02/2015
+ * Time: 19:09
+ */
+
+namespace JDJ\CoreBundle\Behat;
+
+/**
+ * Class UtilsContext
+ * @package AppBundle\Behat
+ */
+class UtilsContext extends DefaultContext
+{
+    /**
+     * @When /^I wait until loading has finished/
+     */
+    public function waitUntilLoadingHasFinished()
+    {
+        $this->getSession()->wait(5000, "$('.spinner').is('visible')");
+        $this->getSession()->wait(5000, "$('.spinner').not('visible')");
+    }
+
+    /**
+     * @When /^I wait "([^""]*)" seconds$/
+     *
+     * @param integer $time
+     */
+    public function waitTime($time)
+    {
+        $this->getSession()->wait($time*1000);
+    }
+
+    /**
+     * @When /^I wait "([^""]*)" seconds until "([^""]*)"$/
+     *
+     * @param integer $time      time in milliseconds
+     * @param string  $condition JS condition
+     */
+    public function waitTimeUntilCondition($time, $condition)
+    {
+        $this->getSession()->wait($time*1000, $condition);
+    }
+
+    /**
+     * @When /^I attach file "([^""]*)" to "([^""]*)"$/
+     *
+     * @param string $path    path to file
+     * @param string $locator input id, name or label
+     *
+     * @throws \Behat\Mink\Exception\ElementNotFoundException
+     */
+    public function attachFile($path, $locator)
+    {
+        $path = realpath($this->getContainer()->getParameter('kernel.root_dir')."/../".$path);
+        $page = $this->getSession()->getPage();
+        $page->attachFileToField($locator, $path);
+    }
+}
