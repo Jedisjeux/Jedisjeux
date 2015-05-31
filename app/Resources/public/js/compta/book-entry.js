d73e3da9bd9da9fa6cf44719e1006c9563a7ddfb
Loic <lc.fremont@gmail.com>
2015-06-01 00:26:21 +0200

    js fix

--- app/Resources/public/js/compta/book-entry.js
+++ app/Resources/public/js/compta/book-entry.js
@@ -1,28 +1,30 @@
 $(function() {
-    if ($('#jdj_comptabundle_book_entry_creditedOrDebited').length > 0) {
 
-        var $creditedFormGroup =  $('#jdj_comptabundle_book_entry_creditedAt').parent().parent();
-        var $debitedFormGroup =  $('#jdj_comptabundle_book_entry_debitedAt').parent().parent();
+    var $creditedFormGroup =  $('#jdj_comptabundle_book_entry_creditedAt').parent().parent();
+    var $debitedFormGroup =  $('#jdj_comptabundle_book_entry_debitedAt').parent().parent();
+    var $selectCreditOrDebit = $('#jdj_comptabundle_book_entry_creditedOrDebited');
+
+    if ($selectCreditOrDebit.length > 0) {
 
         debitedOrCreditedHandler();
     }
 
     function debitedOrCreditedHandler() {
 
-        debitedOrCreditedChange($(this).val());
-
-        $('#jdj_comptabundle_book_entry_creditedOrDebited').change(function() {
+        debitedOrCreditedChange($selectCreditOrDebit.val());
+        $selectCreditOrDebit.change(function() {
             debitedOrCreditedChange($(this).val());
         });
     }
 
     function debitedOrCreditedChange(value) {
         if ('debited' === value) {
+            $debitedFormGroup.hide('slow');
             $creditedFormGroup.show('slow');
-            $debitedFormGroup.hide('slow')
+
         } else {
+            $creditedFormGroup.hide('slow');
             $debitedFormGroup.show('slow');
-            $creditedFormGroup.hide('slow')
         }
     }
 });
