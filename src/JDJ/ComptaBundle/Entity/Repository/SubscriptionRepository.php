1fd6bbdde0d40d9d6703750f8c8c8751b7069251
Loic Fremont <loic@mobizel.com>
2015-11-20 13:59:59 +0100

    Ending subscriptions with and ending date in the past
    Former-commit-id: 3acdd6b1f4acc88913fa302f8d0ae181b3dbbeb4

--- src/JDJ/ComptaBundle/Entity/Repository/SubscriptionRepository.php
+++ src/JDJ/ComptaBundle/Entity/Repository/SubscriptionRepository.php
@@ -9,6 +9,7 @@
 namespace JDJ\ComptaBundle\Entity\Repository;
 
 
+use JDJ\ComptaBundle\Entity\Subscription;
 use JDJ\CoreBundle\Entity\EntityRepository;
 
 /**
@@ -16,5 +17,22 @@ use JDJ\CoreBundle\Entity\EntityRepository;
  */
 class SubscriptionRepository extends EntityRepository
 {
+    /**
+     * @return \Pagerfanta\Pagerfanta
+     */
+    public function findEndingSubscriptions() {
+        $queryBuilder = $this->getQueryBuilder();
 
+        $this->applyCriteria($queryBuilder, array(
+            'status' => Subscription::IN_PROGRESS,
+        ));
+
+        $today = new \DateTime();
+
+        $queryBuilder
+            ->andWhere($queryBuilder->expr()->lt($this->getAlias() . '.endAt', ':today'))
+            ->setParameter('today', $today->format('Y-m-d'));
+
+        return $this->getPaginator($queryBuilder);
+    }
 }
