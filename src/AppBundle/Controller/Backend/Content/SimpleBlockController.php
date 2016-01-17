cfbf3e7f5ff9eecc5cb2f33daa7b4041f23479d5
loic_425 <loic@mobizel.com>
2016-01-17 18:42:13 +0100

    simple block edition in progress
    Former-commit-id: 96d854b5059735a272871fec5c6f5eabb589cd4a

--- src/AppBundle/Controller/Backend/Content/SimpleBlockController.php
+++ src/AppBundle/Controller/Backend/Content/SimpleBlockController.php
@@ -13,10 +13,12 @@ use AppBundle\Form\Type\SimpleBlockType;
 use Doctrine\ODM\PHPCR\Document\Generic;
 use Doctrine\ODM\PHPCR\DocumentManager;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sylius\Bundle\ResourceBundle\Doctrine\ODM\PHPCR\DocumentRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Cmf\Bundle\BlockBundle\Doctrine\Phpcr\SimpleBlock;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 
 /**
@@ -59,11 +61,67 @@ class SimpleBlockController extends Controller
     }
 
     /**
+     * @Route("/{id}/edit", requirements={"id" = ".+"}, name="admin_content_block_simple_edit")
+     *
+     * @param Request $request
+     * @param string $id
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function updateAction(Request $request, $id)
+    {
+        $page = $this->findOr404($id);
+
+        $form = $this->createForm(new PageType(), $page);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getManager();
+            $em->persist($page);
+            $em->flush();
+
+            return $this->redirect($this->generateUrl(
+                'admin_homepage',
+                array('id' => $page->getName())
+            ));
+        }
+
+        return $this->render("backend/content/block/simple/edit.html.twig", array(
+            'block' => $page,
+            'form' => $form->createView(),
+        ));
+    }
+
+    /**
+     * @param string $id
+     * @return SimpleBlock
+     */
+    protected function findOr404($id)
+    {
+        $page = $this
+            ->getRepository()
+            ->find($id);
+
+        if (null === $page) {
+            throw new NotFoundHttpException(sprintf("Block %s not found", $id));
+        }
+
+        return $page;
+    }
+
+    /**
      * @return Generic
      */
     protected function getParent()
     {
         return $this->getManager()->find(null, '/cms/blocks/simple');
+    }
+
+    /**
+     * @return DocumentRepository
+     */
+    public function getRepository()
+    {
+        return $this->get('sylius.repository.simple_block');
     }
 
     /**
