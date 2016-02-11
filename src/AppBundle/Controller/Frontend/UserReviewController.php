4dab6c2bd6a42fd9f7fa649e33b97f789d4dca92
Loic Fremont <loic@mobizel.com>
2016-02-11 14:32:16 +0100

    user reviews in progress
    Former-commit-id: ea6371cbb61c86dc56770dab4c09d53812f20e32

---
+++ src/AppBundle/Controller/Frontend/UserReviewController.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: loic
+ * Date: 10/02/2016
+ * Time: 18:08
+ */
+
+namespace AppBundle\Controller\Frontend;
+
+use JDJ\CoreBundle\Entity\EntityRepository;
+use JDJ\JeuBundle\Entity\Jeu;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * @author Loïc Frémont <loic@mobizel.com>
+ *
+ * @Route("/user-review")
+ */
+class UserReviewController extends Controller
+{
+    /**
+     * @Route("/", name="user_review_index")
+     *
+     * @param Request $request
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function indexAction(Request $request)
+    {
+        $criteria = $request->get('criteria', array());
+        $sorting = $request->get('sorting', array('createdAt' => 'desc'));
+        $template = $request->get('template', 'index.html.twig');
+
+        $userReviews = $this
+            ->getRepository()
+            ->createPaginator($criteria, $sorting)
+            ->setCurrentPage($request->get('page', 1));
+
+        return $this->render('frontend/user_review/'.$template, array(
+            'user_reviews' => $userReviews,
+        ));
+    }
+
+    /**
+     * @return EntityRepository
+     */
+    public function getRepository()
+    {
+        return $this->getDoctrine()->getRepository('JDJUserReviewBundle:UserReview');
+    }
+}
