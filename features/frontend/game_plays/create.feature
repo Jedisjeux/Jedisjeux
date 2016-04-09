b403377974ceb1e04b6ecd9ac9bb1b88e5654f9f
loic_425 <loic@mobizel.com>
2016-04-09 10:58:31 +0200

    add a new game play button on game sheet and adding behat test
    Former-commit-id: 97f964c9232849f52691a05c34ef36cbc04a9c9c

---
+++ features/frontend/game_plays/create.feature
@@ -0,0 +1,22 @@
+@frontend @game_plays @create
+Feature: Game-play creation
+  In order to manage game-plays
+  As a user
+  I need to be able to create new game-plays
+
+  Background:
+    Given there are following users:
+      | email             | password | role      |
+      | kevin@example.com | password | ROLE_USER |
+    And there are products:
+      | name          |
+      | Lewis & Clark |
+    And I am logged in as user "kevin@example.com" with password "password"
+
+  Scenario: Create new game-play
+    Given I am on "/jeu-de-societe/lewis-clark"
+    And I follow "Nouvelle partie"
+    When I fill in the following:
+      | Joué le | 2016-04-01 |
+    And I press "Créer"
+    Then I should see "a bien été créé"
---
+++ src/AppBundle/Behat/ProductContext.php
@@ -0,0 +1,115 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: loic
+ * Date: 14/03/2016
+ * Time: 11:14
+ */
+
+namespace AppBundle\Behat;
+
+use AppBundle\Entity\Product;
+use Behat\Gherkin\Node\TableNode;
+use Sylius\Component\Attribute\AttributeType\CheckboxAttributeType;
+use Sylius\Component\Attribute\Model\AttributeInterface;
+use Sylius\Component\Attribute\Model\AttributeValueInterface;
+use Sylius\Component\Product\Model\ProductInterface;
+
+/**
+ * @author LoÃ¯c FrÃ©mont <loic@mobizel.com>
+ */
+class ProductContext extends DefaultContext
+{
+    /**
+     * @Given /^there are products:$/
+     * @Given /^there are following products:$/
+     * @Given /^the following products exist:$/
+     *
+     * @param TableNode $table
+     */
+    public function thereAreProducts(TableNode $table)
+    {
+        $manager = $this->getEntityManager();
+
+        foreach ($table->getHash() as $data) {
+
+            /** @var Product $product */
+            $product = $this->getFactory('product')->createNew();
+            $product->setCode($this->faker->unique()->postcode);
+            $product->setName(isset($data['name']) ? $data['name'] : $this->faker->name);
+            $product->setDescription(isset($data['description']) ? $data['description'] : $this->faker->realText());
+
+            $manager->persist($product);
+        }
+
+        $manager->flush();
+    }
+
+    /**
+     * @Given /^product "([^"]*)" has following attributes:$/
+     */
+    public function productHasAttributes($productName, TableNode $table)
+    {
+        /** @var ProductInterface $product */
+        $product = $this->findOneByName('product', $productName);
+
+        foreach ($table->getHash() as $data) {
+            /**
+             * @var AttributeInterface $productAttribute
+             * @var AttributeValueInterface $attributeValue
+             */
+            $productAttribute = $this->findOneByName('product_attribute', trim($data['name']));
+            $attributeValue = $this->getFactory('product_attribute_value')->createNew();
+
+            $attributeValue->setAttribute($productAttribute);
+            $attributeValue->setValue($data['value']);
+
+            $product->addAttribute($attributeValue);
+        }
+
+        $this->getEntityManager()->flush();
+    }
+
+    /**
+     * @Given /^there are following attributes:$/
+     * @Given /^the following attributes exist:$/
+     */
+    public function thereAreAttributes(TableNode $table)
+    {
+        foreach ($table->getHash() as $attribute) {
+            $this->thereIsAttribute(
+                $attribute['name'],
+                $attribute['type'],
+                (isset($attribute['code'])) ? $attribute['code'] : null,
+                (isset($attribute['configuration'])) ? $attribute['configuration'] : null
+            );
+        }
+
+        $this->getEntityManager()->flush();
+    }
+
+    /**
+     * @Given /^There is attribute "([^""]*)" with type "([^""]*)"$/
+     * @Given /^I created attribute "([^""]*)" with type "([^""]*)"$/
+     */
+    public function thereIsAttribute($name, $type, $code = null, $configuration = null)
+    {
+        $code = (null === $code) ? strtolower(str_replace(' ', '_', $name)) : $code;
+        $storageType = (CheckboxAttributeType::TYPE === $type) ? 'boolean' : $type;
+
+        $attribute = $this->getFactory('product_attribute')->createNew();
+        $attribute->setName($name);
+        $attribute->setType($type);
+        $attribute->setCode($code);
+        $attribute->setStorageType($storageType);
+
+        if (null !== $configuration && '' !== $configuration) {
+            $attribute->setConfiguration($this->getConfiguration($configuration));
+        }
+
+        $manager = $this->getEntityManager();
+        $manager->persist($attribute);
+
+        return $attribute;
+    }
+}
