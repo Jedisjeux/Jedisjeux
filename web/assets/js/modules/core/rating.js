9c59228344b61f4cc2004196b978b5ce2771db54
Loic Fremont <loic@mobizel.com>
2016-04-26 13:54:20 +0200

    theming product review form
    Former-commit-id: 4911efcb8acdd3ae397dba8cd9ed5bee7b33efac

--- web/assets/js/modules/core/rating.js
+++ web/assets/js/modules/core/rating.js
@@ -3,6 +3,7 @@ $(function() {
     "use strict";
 
     $(".rating").rate({
+        selected_symbol_type: 'fontawesome_star',
         update_input_field_name: $("#sylius_product_review_rating")
     });
 
