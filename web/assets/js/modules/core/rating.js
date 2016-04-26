af7660f2bf65e49e3c27efda3785a5953f527033
loic_425 <loic@mobizel.com>
2016-04-27 01:21:00 +0200

    show ratingAverage on product sheet
    Former-commit-id: 88218955210f061c3bdce53182d03f0e947de4bf

--- web/assets/js/modules/core/rating.js
+++ web/assets/js/modules/core/rating.js
@@ -2,9 +2,13 @@ $(function() {
 
     "use strict";
 
-    $(".rating").rate({
-        selected_symbol_type: 'fontawesome_star',
-        update_input_field_name: $("#sylius_product_review_rating")
+    $(".rating").each(function() {
+        $(this).rate({
+            max_value: $(this).attr("data-max-value"),
+            readonly: $(this).attr("data-readonly"),
+            selected_symbol_type: 'fontawesome_star',
+            update_input_field_name: $("#sylius_product_review_rating")
+        });
     });
 
 });
