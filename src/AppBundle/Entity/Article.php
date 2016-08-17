9ed9916d5325c8a2caa4f49a07edbcd337395b48
Loic Fremont <loic@mobizel.com>
2016-08-17 11:41:46 +0200

    adding view count property on articles
    Former-commit-id: 587fa8869d8754e1ad4479d6c7e9fb7458d33ee2

--- src/AppBundle/Entity/Article.php
+++ src/AppBundle/Entity/Article.php
@@ -90,6 +90,13 @@ class Article implements ResourceInterface, ReviewableInterface
     protected $publishable = true;
 
     /**
+     * @var int
+     *
+     * @ORM\Column(type="integer")
+     */
+    protected $viewCount = 0;
+
+    /**
      * @var ProductInterface
      *
      * @ORM\ManyToOne(targetEntity="Sylius\Component\Product\Model\ProductInterface")
@@ -486,6 +493,26 @@ class Article implements ResourceInterface, ReviewableInterface
     public function setPublishable($publishable)
     {
         $this->publishable = $publishable;
+
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getViewCount()
+    {
+        return $this->viewCount;
+    }
+
+    /**
+     * @param int $viewCount
+     *
+     * @return $this
+     */
+    public function setViewCount($viewCount)
+    {
+        $this->viewCount = $viewCount;
 
         return $this;
     }
