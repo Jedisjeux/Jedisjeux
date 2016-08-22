782ed30232f2c14fd31cc8fbadfd615379881dab
Loic Fremont <loic@mobizel.com>
2016-08-22 16:09:29 +0200

    adding filter on article status on backend article index page
    Former-commit-id: 087b00a15b39ecde622cf119e3079bef31e42f04

---
+++ src/AppBundle/Form/Filter/ArticleFilterType.php
@@ -0,0 +1,66 @@
+<?php
+
+/*
+ * This file is part of Jedisjeux project.
+ *
+ * (c) Jedisjeux
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace AppBundle\Form\Filter;
+
+use AppBundle\Entity\Article;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+/**
+ * @author Loïc Frémont <loic@mobizel.com>
+ */
+class ArticleFilterType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('query', 'text', array(
+                'label' => 'label.search',
+                'required' => false,
+            ))
+            ->add('status', 'choice', array(
+                'label' => 'label.show_only_suggestions_with_status',
+                'required' => false,
+                'choices' => [
+                    'label.new' => Article::STATUS_NEW,
+                    'label.need_a_review' => Article::STATUS_NEED_A_REVIEW,
+                    'label.ready_to_publish' => Article::STATUS_READY_TO_PUBLISH,
+                    'label.published' => Article::STATUS_PUBLISHED,
+                ],
+                'choices_as_values' => true,
+            ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver
+            ->setDefaults(array(
+                'data_class' => null,
+                'criteria' => null,
+            ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return 'app_filter_article';
+    }
+}
