59b74ffed210b4c0c5ee90ab57ef9d3834bcced4
Loic Fremont <loic@mobizel.com>
2016-09-20 18:25:05 +0200

    view product reviews of a person on ludography show page
    Former-commit-id: 713c480b153492f8f8484b35523c9c9dacdc14dc

--- src/AppBundle/Repository/ProductReviewRepository.php
+++ src/AppBundle/Repository/ProductReviewRepository.php
@@ -9,6 +9,7 @@
 namespace AppBundle\Repository;
 
 use Doctrine\ORM\QueryBuilder;
+use Pagerfanta\Pagerfanta;
 use Sylius\Bundle\ResourceBundle\Doctrine\ORM\EntityRepository;
 use Sylius\Component\Review\Model\ReviewInterface;
 
@@ -17,6 +18,18 @@ use Sylius\Component\Review\Model\ReviewInterface;
  */
 class ProductReviewRepository extends EntityRepository
 {
+    /**
+     * @return QueryBuilder
+     */
+    protected function getQueryBuilder()
+    {
+        return $this->createQueryBuilder('o')
+            ->select('o, product, productVariant, productTranslation')
+            ->join('o.reviewSubject', 'product')
+            ->leftJoin('product.variants', 'productVariant')
+            ->leftJoin('product.translations', 'productTranslation');
+    }
+
     /**
      * {@inheritdoc}
      */
@@ -35,6 +48,47 @@ class ProductReviewRepository extends EntityRepository
 
 
         parent::applyCriteria($queryBuilder, $criteria);
+    }
+
+    /**
+     * Create filter paginator.
+     *
+     * @param array $criteria
+     * @param array $sorting
+     *
+     * @return Pagerfanta
+     */
+    public function createFilterPaginator($criteria = [], $sorting = [])
+    {
+        $queryBuilder = $this->getQueryBuilder();
+
+        if (isset($criteria['person'])) {
+            $queryBuilder
+                ->leftJoin('productVariant.designers', 'designer')
+                ->leftJoin('productVariant.artists', 'artist')
+                ->leftJoin('productVariant.publishers', 'publisher')
+                ->andWhere($queryBuilder->expr()->orX(
+                    'designer = :person',
+                    'artist = :person',
+                    'publisher = :person'
+                ))
+                ->setParameter('person', $criteria['person']);
+
+            unset($criteria['person']);
+        }
+
+        if (empty($sorting)) {
+            if (!is_array($sorting)) {
+                $sorting = [];
+            }
+
+            $sorting['updatedAt'] = 'desc';
+        }
+
+        $this->applyCriteria($queryBuilder, (array)$criteria);
+        $this->applySorting($queryBuilder, $sorting);
+
+        return $this->getPaginator($queryBuilder);
     }
 
     /**
