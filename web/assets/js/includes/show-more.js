96bbe774dabe0967a5467d83f23732af4858961d
loic_425 <loic@mobizel.com>
2016-09-22 01:24:57 +0200

    improve taxon list on sidebar with js
    Former-commit-id: 88059444a546d31207f8b6f13faec0e7f39f9913

--- web/assets/js/includes/show-more.js
+++ web/assets/js/includes/show-more.js
@@ -19,5 +19,23 @@ $(function () {
         $this.attr('data-closed', '1');
       }
     });
+
+    $("a.show-more-2").on("click", function (event) {
+      event.preventDefault();
+
+      var $this = $(this);
+      var $content = $this.parent();
+
+      if ($this.attr('data-closed')) {
+        $('li', $content).show('slow');
+        $this.removeAttr('data-closed');
+        $this.html('<span class="fa fa-chevron-up"></span> ' + $this.attr('data-close-label'));
+      } else {
+        $('li.closed', $content).hide('slow');
+        $this.attr('data-closed', '1');
+        $this.html('<span class="fa fa-chevron-down"></span> ' + $this.attr('data-open-label'));
+      }
+    });
+
   });
 });
