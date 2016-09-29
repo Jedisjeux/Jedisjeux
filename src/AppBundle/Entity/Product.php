30f2400cfc2600ccf132130c0784574069b3f473
Loic Fremont <loic@mobizel.com>
2016-09-29 13:40:59 +0200

    fix code of product can be null
    Former-commit-id: 45124e136fd21cabea6cfde8575c0b2dd4a840be

--- src/AppBundle/Entity/Product.php
+++ src/AppBundle/Entity/Product.php
@@ -47,7 +47,7 @@ class Product extends BaseProduct implements ReviewableInterface
     /**
      * @var string
      *
-     * @ORM\Column(type="string", unique=true)
+     * @ORM\Column(type="string", unique=true, nullable=true)
      */
     protected $code;
 
