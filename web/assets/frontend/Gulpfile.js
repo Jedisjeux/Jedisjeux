e0d579032b6d4683e96f2e16db0e95e9bc3cb228
Loic Fremont <loicfremont@MacBook-Pro-de-Loic.local>
2016-12-03 23:56:04 +0100

    fix gulp
    Former-commit-id: 8cae25925ddc37dda7ffed3a9bdb6a7f5872add8

--- web/assets/frontend/Gulpfile.js
+++ web/assets/frontend/Gulpfile.js
@@ -17,22 +17,32 @@ var vendorPath = argv.vendorPath || '';
 var vendorUiPath = '' === vendorPath ? '../../../vendor/sylius/ui-bundle/' : vendorPath + 'sylius/ui-bundle/';
 var nodeModulesPath = argv.nodeModulesPath;
 var bundlesPath = rootPath + '../bundles/';
+var bowerComponentsPath = rootPath + 'frontend/bower_components/';
 
 var paths = {
     shop: {
         js: [
             nodeModulesPath + 'jquery/dist/jquery.min.js',
             bundlesPath + 'bmatznerjqueryui/js/minified/jquery-ui.min.js',
-            nodeModulesPath + 'bootstrap-star-rating/js/star-rating.min.js',
-            nodeModulesPath + 'fancybox/dist/js/jquery.fancybox.pack.js',
-            nodeModulesPath + 'fancybox/dist/helpers/jquery.fancybox-buttons.js',
-            nodeModulesPath + 'fancybox/dist/helpers/jquery.fancybox-media.js',
-            nodeModulesPath + 'fancybox/dist/helpers/jquery.fancybox-thumbs.js',
-            nodeModulesPath + 'multiple-select/multiple.select.js',
-            nodeModulesPath + 'owlcarousel/dist/owl.carousel.js',
-            nodeModulesPath + 'select2/dist/js/select2.js',
-            nodeModulesPath + 'jquery-countTo/build/jquery.countTo.js',
-            nodeModulesPath + 'rater/index.js',
+            bundlesPath + 'mopabootstrap/bootstrap-sass/assets/javascripts/bootstrap/tooltip.js',
+            bundlesPath + 'mopabootstrap/bootstrap-sass/assets/javascripts/bootstrap/*.js',
+            bundlesPath + 'mopabootstrap/js/mopabootstrap-collection.js',
+            bundlesPath + 'mopabootstrap/js/mopabootstrap-subnav.js',
+            bowerComponentsPath + 'bootstrap-star-rating/js/star-rating.min.js',
+            bowerComponentsPath + 'fancybox/lib/jquery.mousewheel-3.0.6.pack.js',
+            bowerComponentsPath + 'fancybox/source/jquery.fancybox.pack.js',
+            bowerComponentsPath + 'fancybox/source/helpers/jquery.fancybox-buttons.js',
+            bowerComponentsPath + 'fancybox/source/helpers/jquery.fancybox-media.js',
+            bowerComponentsPath + 'fancybox/source/helpers/jquery.fancybox-thumbs.js',
+            bowerComponentsPath + 'multiple-select/jquery.multiple.select.js',
+            bowerComponentsPath + 'magnific-popup/dist/jquery.magnific-popup.min.js',
+            bowerComponentsPath + 'moment/min/moment.min.js',
+            bowerComponentsPath + 'eonasdan-bootstrap-datetimepicker/build/js/bootstrap-datetimepicker.min.js',
+            bowerComponentsPath + 'owlcarousel/owl-carousel/owl.carousel.js',
+            bowerComponentsPath + 'select2/dist/js/select2.js',
+            bowerComponentsPath + 'jquery-countTo/jquery.countTo.js',
+            bowerComponentsPath + 'rater/rater.js',
+            bowerComponentsPath + 'bootstrap-switch/dist/js/bootstrap-switch.min.js',
             'js/**'
         ],
         sass: [
@@ -42,15 +52,16 @@ var paths = {
         css: [
             'css/magnific-popup.css',
             bundlesPath + 'bmatznerfontawesome/css/font-awesome.css',
-            nodeModulesPath + 'jquery-ui/themes/base/all.css',
-            nodeModulesPath + 'bootstrap-star-rating/css/star-rating.min.css',
-            nodeModulesPath + 'fancybox/dist/css/jquery.fancybox.css',
-            nodeModulesPath + 'fancybox/dist/helpers/css/jquery.fancybox-buttons.css',
-            nodeModulesPath + 'fancybox/dist/helpers/css/jquery.fancybox-thumbs.css',
-            nodeModulesPath + 'multiple-select/multiple-select.css',
-            nodeModulesPath + 'owl.carousel/dist/assets/owl.carousel.css',
-            nodeModulesPath + 'select2/dist/css/select2.css',
-            nodeModulesPath + 'bootstrap-switch/dist/css/bootstrap3/bootstrap-switch.min.css'
+            bundlesPath + 'bmatznerjqueryui/css/smoothness/jquery-ui.css',
+            bowerComponentsPath + 'bootstrap-star-rating/css/star-rating.min.css',
+            bowerComponentsPath + 'fancybox/source/jquery.fancybox.css',
+            bowerComponentsPath + 'fancybox/source/helpers/jquery.fancybox-buttons.css',
+            bowerComponentsPath + 'fancybox/source/helpers/jquery.fancybox-thumbs.css',
+            bowerComponentsPath + 'multiple-select/multiple-select.css',
+            bowerComponentsPath + 'eonasdan-bootstrap-datetimepicker/build/css/bootstrap-datetimepicker.min.css',
+            bowerComponentsPath + 'owlcarousel/owl-carousel/owl.carousel.css',
+            bowerComponentsPath + 'select2/dist/css/select2.css',
+            bowerComponentsPath + 'bootstrap-switch/dist/css/bootstrap3/bootstrap-switch.min.css'
         ],
         img: [
             vendorUiPath + 'Resources/private/img/**',
