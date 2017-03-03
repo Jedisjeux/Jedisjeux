5ef6b6dcd94fa223c8083e790d48a12b8a1e0321
Loic Fremont <loic@mobizel.com>
2017-03-03 15:03:00 +0100

    add download article main images on download image command
    Former-commit-id: eb6c487c1e1fd54cb204b7a604a2489c764698ce

--- src/AppBundle/Command/Installer/Data/DownloadImageCommand.php
+++ src/AppBundle/Command/Installer/Data/DownloadImageCommand.php
@@ -53,6 +53,11 @@ class DownloadImageCommand extends ContainerAwareCommand
         $repository = $this->getContainer()->get('app.repository.person_image');
         $queryBuilder = $repository->createQueryBuilder('o');
         $this->downloadImages($queryBuilder);
+
+        /** @var EntityRepository $repository */
+        $repository = $this->getContainer()->get('app.repository.article_image');
+        $queryBuilder = $repository->createQueryBuilder('o');
+        $this->downloadImages($queryBuilder);
     }
 
     /**
