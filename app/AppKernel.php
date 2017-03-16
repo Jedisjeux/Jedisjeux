0b5977f1006d6f90f0d17df82ad52756d3c35a48
Loic Fremont <loic@mobizel.com>
2017-03-16 17:34:41 +0100

    add api test case
    Former-commit-id: 89440ca6dd0322b6624b413f50572c5047cc04aa

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -1,5 +1,6 @@
 <?php
 
+use PSS\SymfonyMockerContainer\DependencyInjection\MockerContainer;
 use Symfony\Component\HttpKernel\Kernel;
 use Symfony\Component\Config\Loader\LoaderInterface;
 
@@ -87,6 +88,18 @@ class AppKernel extends Kernel
         }
 
         return $bundles;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getContainerBaseClass()
+    {
+        if (in_array($this->getEnvironment(), ['test', 'test_cached'], true)) {
+            return MockerContainer::class;
+        }
+
+        return parent::getContainerBaseClass();
     }
 
     public function registerContainerConfiguration(LoaderInterface $loader)
---
+++ tests/Controller/ProductApiTest.php
@@ -0,0 +1,63 @@
+<?php
+
+/*
+ * This file is part of jdj.
+ *
+ * (c) Mobizel
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace tests\Controller;
+
+use Lakion\ApiTestCase\JsonApiTestCase;
+use Sylius\Component\Product\Model\ProductInterface;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * @author Loïc Frémont <loic@mobizel.com>
+ */
+class ProductApiTest extends JsonApiTestCase
+{
+    /**
+     * @var array
+     */
+    private static $authorizedHeaderWithContentType = [
+        'HTTP_Authorization' => 'Bearer SampleTokenNjZkNjY2MDEwMTAzMDkxMGE0OTlhYzU3NzYyMTE0ZGQ3ODcyMDAwM2EwMDZjNDI5NDlhMDdlMQ',
+        'CONTENT_TYPE' => 'application/json',
+    ];
+
+    /**
+     * @var array
+     */
+    private static $authorizedHeaderWithAccept = [
+        'HTTP_Authorization' => 'Bearer SampleTokenNjZkNjY2MDEwMTAzMDkxMGE0OTlhYzU3NzYyMTE0ZGQ3ODcyMDAwM2EwMDZjNDI5NDlhMDdlMQ',
+        'ACCEPT' => 'application/json',
+    ];
+
+
+    /**
+     * @test
+     */
+    public function it_allows_showing_product()
+    {
+        $products = $this->loadFixturesFromFile('resources/products.yml');
+        $product = $products['product1'];
+
+        $this->client->request('GET', $this->getProductUrl($product), [], [], static::$authorizedHeaderWithAccept);
+
+        $response = $this->client->getResponse();
+        $this->assertResponse($response, 'product/show_response', Response::HTTP_OK);
+    }
+
+    /**
+     * @param ProductInterface $product
+     *
+     * @return string
+     */
+    private function getProductUrl(ProductInterface $product)
+    {
+        return '/api/products/' . $product->getId();
+    }
+}
