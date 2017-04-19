9cda1640feb34f91c740f81953d1be750267bc7f
Loic Fremont <lc.fremont@gmail.com>
2017-04-19 13:38:36 +0200

    fix update a customer
    Former-commit-id: 2b352442473a451bad1e78068efd61f10c1684f9

--- src/AppBundle/Form/Type/Customer/CustomerType.php
+++ src/AppBundle/Form/Type/Customer/CustomerType.php
@@ -12,8 +12,8 @@
 namespace AppBundle\Form\Type\Customer;
 
 use Sylius\Bundle\CustomerBundle\Form\Type\CustomerType as BaseCustomerType;
-use Sylius\Bundle\ResourceBundle\Form\Type\AbstractResourceType;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
@@ -23,27 +23,8 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 /**
  * @author Michał Marcinkowski <michal.marcinkowski@lakion.com>
  */
-class CustomerType extends AbstractResourceType
+class CustomerType extends AbstractType
 {
-    /**
-     * @var EventSubscriberInterface
-     */
-    private $addUserFormSubscriber;
-
-    /**
-     * @param string $dataClass
-     * @param string[] $validationGroups
-     * @param EventSubscriberInterface $addUserFormSubscriber
-     */
-    public function __construct(
-        $dataClass,
-        array $validationGroups = [],
-        EventSubscriberInterface $addUserFormSubscriber
-    ) {
-        parent::__construct($dataClass, $validationGroups);
-        $this->addUserFormSubscriber = $addUserFormSubscriber;
-    }
-
     /**
      * {@inheritdoc}
      */
@@ -53,19 +34,8 @@ class CustomerType extends AbstractResourceType
 
         $builder
             ->remove('gender')
-            ->add('phoneNumber', TextType::class, [
-                'label' => 'sylius.ui.phone_number',
-                'required' => false,
-            ])
-            ->add('firstName', HiddenType::class, [
-                'data' => 'John',
-            ])
-            ->add('lastName', HiddenType::class, [
-                'data' => 'Doe',
-            ]);
-
-        $builder
-            ->addEventSubscriber($this->addUserFormSubscriber);
+            ->add('firstName', HiddenType::class)
+            ->add('lastName', HiddenType::class);
     }
 
     /**
@@ -74,17 +44,5 @@ class CustomerType extends AbstractResourceType
     public function getParent()
     {
         return BaseCustomerType::class;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function configureOptions(OptionsResolver $resolver)
-    {
-        $resolver->setDefaults([
-            'data_class' => $this->dataClass,
-            'validation_groups' => $this->validationGroups,
-            'cascade_validation' => true,
-        ]);
     }
 }
