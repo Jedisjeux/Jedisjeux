587bdfb166230fde2ae97ac91f89beadd71cfcda
Loic Fremont <lc.fremont@gmail.com>
2017-05-24 21:50:43 +0200

    fix update view count from google analytics
    Former-commit-id: 6466c5bcc6e99e2199d3c6345922149f7d1842bd

--- src/AppBundle/Form/Type/TopicViewCountType.php
+++ src/AppBundle/Form/Type/TopicViewCountType.php
@@ -11,6 +11,9 @@
 
 namespace AppBundle\Form\Type;
 
+use AppBundle\Entity\Topic;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
 /**
  * @author Loïc Frémont <loic@mobizel.com>
  */
@@ -19,7 +22,17 @@ class TopicViewCountType extends AbstractViewCountType
     /**
      * {@inheritdoc}
      */
-    public function getName()
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => Topic::class,
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
     {
         return 'app_topic_view_count';
     }
