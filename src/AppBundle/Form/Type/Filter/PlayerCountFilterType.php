d2815c68e8f3b8a3e0534832b7889aca508a89f6
Loic Fremont <lc.fremont@gmail.com>
2017-07-24 09:41:11 +0200

    create player count filter
    Former-commit-id: 3cbc70fe2ce826b451b7be4b769442877c053333

---
+++ src/AppBundle/Form/Type/Filter/PlayerCountFilterType.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * This file is part of Jedisjeux
+ *
+ * (c) Loïc Frémont
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace AppBundle\Form\Type\Filter;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+/**
+ * @author Loïc Frémont <loic@mobizel.com>
+ */
+class PlayerCountFilterType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add(
+            'value',
+            IntegerType::class
+        );
+    }
+}
