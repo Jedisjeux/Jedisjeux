6205a11498d43ffe16fabed1a20c90005c1626f2
Loic Fremont <loic@mobizel.com>
2017-08-25 15:18:18 +0200

    add tab errors
    Former-commit-id: 05b60665df70b47d04e52ccfb1060bacc1e2eada

--- web/assets/backend/compiled/js/app.js
+++ web/assets/backend/compiled/js/app.js
@@ -6361,6 +6361,8 @@ S2.define('jquery.select2',[
 (function($) {
     $(document).ready(function () {
         $('.sylius-autocomplete').autoComplete();
+        $('.sylius-tabular-form').addTabErrors();
+        $('.ui.accordion').addAccordionErrors();
         $('.product-select.ui.fluid.multiple.search.selection.dropdown').productAutoComplete();
         $('.sylius-update-product-variants').moveProductVariant($('.sylius-product-variant-position'));
         $(document).productSlugGenerator();
@@ -6632,6 +6634,53 @@ $(function () {
   }
 
 });
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+(function ($) {
+    'use strict';
+
+    $.fn.extend({
+        addTabErrors: function () {
+            var element = $(this);
+
+            $('.ui.segment > .ui.tab').each(function () {
+                var errors = $(this).find('.sylius-validation-error');
+
+                if(0 !== errors.length) {
+                    var tabName = $(this).attr('data-tab');
+                    var tabWithErrors = $(element).find('a.item[data-tab="' + tabName + '"]');
+
+                    var label = tabWithErrors.html();
+                    var newLabel = label + '<span class="ui small horizontal circular label" style="background-color: #DB2828">' + errors.length + '</span>';
+
+                    tabWithErrors.html(newLabel);
+                }
+            });
+        },
+        addAccordionErrors: function () {
+            var element = $(this);
+            var accordionElements = element.find('.ui.content');
+
+            $(accordionElements).each(function () {
+                var errors = $(this).find('.sylius-validation-error');
+
+                if(0 !== errors.length) {
+                    var ribWithErrors = $(this).closest('[data-locale]').find('.title');
+
+                    ribWithErrors.css('color', '#DB2828');
+                }
+            });
+        }
+    })
+})( jQuery );
+
 /*
  * This file is part of the Sylius package.
  *
