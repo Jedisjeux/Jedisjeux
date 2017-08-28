5cab086505c7ac7cd50dfc0424adee5bd5c144e3
Loic Fremont <lc.fremont@gmail.com>
2017-08-28 23:15:38 +0200

    adding product list fixtures
    Former-commit-id: 905387fa9013717054ab4731f429b89b8b42a2b4

---
+++ src/AppBundle/Fixture/ProductListFixture.php
@@ -0,0 +1,41 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace AppBundle\Fixture;
+
+use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
+
+/**
+ * @author Loïc Frémont <lc.fremont@gmail.com>
+ */
+class ProductListFixture extends AbstractResourceFixture
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return 'product_list';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function configureResourceNode(ArrayNodeDefinition $resourceNode)
+    {
+        $resourceNode
+            ->children()
+                ->scalarNode('name')->cannotBeEmpty()->end()
+                ->scalarNode('code')->cannotBeEmpty()->end()
+                ->scalarNode('owner')->cannotBeEmpty()->end()
+        ;
+    }
+}
