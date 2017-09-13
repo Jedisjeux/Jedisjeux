291f79793b0a5c5184493a68092f364af012e628
Loic Fremont <loic@mobizel.com>
2017-09-13 14:20:30 +0200

    WIP managing festival list items on backend
    Former-commit-id: abff02f5af6c61bb3929d437e52de9d799e6546f

---
+++ src/AppBundle/Form/Type/FestivalListItemType.php
@@ -0,0 +1,62 @@
+<?php
+
+/**
+ * This file is part of Jedisjeux
+ *
+ * (c) Loïc Frémont
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace AppBundle\Form\Type;
+
+use AppBundle\Entity\FestivalList;
+use AppBundle\Entity\FestivalListItem;
+use AppBundle\Entity\Product;
+use Ivory\CKEditorBundle\Form\Type\CKEditorType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+/**
+ * @author Loïc Frémont <loic@mobizel.com>
+ */
+class FestivalListItemType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        parent::buildForm($builder, $options);
+
+        $builder
+            ->add('product', EntityType::class, [
+                'class' => Product::class,
+            ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        parent::configureOptions($resolver);
+
+        $resolver->setDefaults(array(
+            'data_class' => FestivalListItem::class,
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'app_festival_list_item';
+    }
+}
