2786add3282a76b41e8cfe2540f5b634456706c2
Loic Fremont <loic@mobizel.com>
2017-09-29 11:54:02 +0200

    fix festival list fixture
    Former-commit-id: ededa0f704977e83ef2dd850362d78a2556796d8

--- src/AppBundle/Fixture/FestivalListFixture.php
+++ src/AppBundle/Fixture/FestivalListFixture.php
@@ -21,7 +21,7 @@ class FestivalListFixture extends AbstractResourceFixture
     /**
      * {@inheritdoc}
      */
-    public function getName()
+    public function getName(): string
     {
         return 'festival_list';
     }
