dfa7303f673c5e3276fc2f95d3e48c5d38837525
Loic Fremont <lc.fremont@gmail.com>
2017-10-04 22:29:56 +0200

    improve spec of article entity

--- spec/AppBundle/Entity/ArticleSpec.php
+++ spec/AppBundle/Entity/ArticleSpec.php
@@ -3,10 +3,12 @@
 namespace spec\AppBundle\Entity;
 
 use AppBundle\Entity\Article;
+use AppBundle\Entity\ArticleImage;
 use AppBundle\Entity\Topic;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
 use Sylius\Component\Resource\Model\ResourceInterface;
+use Sylius\Component\Taxonomy\Model\TaxonInterface;
 
 class ArticleSpec extends ObjectBehavior
 {
@@ -35,6 +37,61 @@ class ArticleSpec extends ObjectBehavior
     {
         $this->setTitle("Awesome title");
         $this->getTitle()->shouldReturn("Awesome title");
+    }
+
+    function it_has_no_publish_start_date_by_default()
+    {
+        $this->getPublishStartDate()->shouldReturn(null);
+    }
+
+    function its_publish_start_date_is_mutable(\DateTime $startDate)
+    {
+        $this->setPublishStartDate($startDate);
+        $this->getPublishStartDate()->shouldReturn($startDate);
+    }
+
+    function it_has_no_publish_end_date_by_default()
+    {
+        $this->getPublishEndDate()->shouldReturn(null);
+    }
+
+    function its_publish_end_date_is_mutable(\DateTime $endDate)
+    {
+        $this->setPublishEndDate($endDate);
+        $this->getPublishEndDate()->shouldReturn($endDate);
+    }
+
+    function its_status_is_new_by_default()
+    {
+        $this->getStatus()->shouldReturn(Article::STATUS_NEW);
+    }
+
+    function its_status_is_mutable()
+    {
+        $this->setStatus(Article::STATUS_PENDING_PUBLICATION);
+        $this->getStatus()->shouldReturn(Article::STATUS_PENDING_PUBLICATION);
+    }
+
+    function it_has_no_main_image_by_default()
+    {
+        $this->getMainImage()->shouldReturn(null);
+    }
+
+    function its_main_image_is_mutable(ArticleImage $image)
+    {
+        $this->setMainImage($image);
+        $this->getMainImage()->shouldReturn($image);
+    }
+
+    function it_has_no_main_taxon_by_default()
+    {
+        $this->getMainTaxon()->shouldReturn(null);
+    }
+
+    function its_main_taxon_is_mutable(TaxonInterface $taxon)
+    {
+        $this->setMainTaxon($taxon);
+        $this->getMainTaxon()->shouldReturn($taxon);
     }
 
     function it_has_no_topic_by_default()
