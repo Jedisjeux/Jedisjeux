85b0c77039e0821aae0ecbc4ce0be656e4e3ae5b
Loic Fremont <lc.fremont@gmail.com>
2018-04-30 16:10:35 +0200

    fix yarn check

--- etc/travis/suites/application/before_install.sh
+++ etc/travis/suites/application/before_install.sh
@@ -12,14 +12,3 @@ run_command "sudo apt-get install -y chromium-browser"
 print_header "Installing elasticsearch" "Jedisjeux"
 run_command "curl -O https://download.elastic.co/elasticsearch/elasticsearch/elasticsearch-1.7.3.deb && sudo dpkg -i --force-confnew elasticsearch-1.7.3.deb && sudo service elasticsearch restart"
 run_command "sleep 10"
-
-print_header "Installing Yarn" "Jedisjeux"
-
-# Install Node Version Manager to install newer node version
-run_command "rm -rf ~/.nvm && git clone https://github.com/creationix/nvm.git ~/.nvm && (cd ~/.nvm && git checkout \`git describe --abbrev=0 --tags\`) && source ~/.nvm/nvm.sh && nvm install $TRAVIS_NODE_VERSION" || exit $?
-
-# Install Yarn globally
-run_command "sudo apt-key adv --fetch-keys http://dl.yarnpkg.com/debian/pubkey.gpg"
-run_command "echo \"deb http://dl.yarnpkg.com/debian/ stable main\" | sudo tee /etc/apt/sources.list.d/yarn.list"
-run_command "sudo apt-get update -qq"
-run_command "sudo apt-get install -y -qq yarn=0.21.3-1"
