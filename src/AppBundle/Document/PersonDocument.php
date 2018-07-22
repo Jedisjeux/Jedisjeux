02f36902a604f1cb7d5f7df8bf0792f92ff1b334
Loic Fremont <lc.fremont@gmail.com>
2018-07-22 15:42:16 +0200

    add person document

---
+++ src/AppBundle/Document/PersonDocument.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * This file is part of Jedisjeux
+ *
+ * (c) Loïc Frémont
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace AppBundle\Document;
+
+use ONGR\ElasticsearchBundle\Annotation as ElasticSearch;
+
+/**
+ * @ElasticSearch\Object
+ */
+class PersonDocument
+{
+    /**
+     * @var int
+     *
+     * @ElasticSearch\Property(type="keyword")
+     */
+    private $id;
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+}
