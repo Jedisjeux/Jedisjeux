d4b6b7f09674aba6093dc08df89729e0d4837a21
Loic Fremont <lc.fremont@gmail.com>
2018-07-22 15:36:30 +0200

    fix phpspec

--- spec/AppBundle/Factory/Document/TopicDocumentFactorySpec.php
+++ spec/AppBundle/Factory/Document/TopicDocumentFactorySpec.php
@@ -4,17 +4,20 @@ namespace spec\AppBundle\Factory\Document;
 
 use AppBundle\Document\AppDocument;
 use AppBundle\Document\TopicDocument;
+use AppBundle\Entity\Customer;
 use AppBundle\Entity\Topic;
+use AppBundle\Factory\Document\ImageDocumentFactory;
 use AppBundle\Factory\Document\TopicDocumentFactory;
 use PhpSpec\ObjectBehavior;
 
 class TopicDocumentFactorySpec extends ObjectBehavior
 {
-    function let()
+    function let(ImageDocumentFactory $imageDocumentFactory)
     {
         $this->beConstructedWith(
             AppDocument::class,
-            TopicDocument::class
+            TopicDocument::class,
+            $imageDocumentFactory
         );
     }
 
@@ -23,11 +26,12 @@ class TopicDocumentFactorySpec extends ObjectBehavior
         $this->shouldHaveType(TopicDocumentFactory::class);
     }
 
-    function it_creates_document_from_topic(Topic $topic): void
+    function it_creates_document_from_topic(Topic $topic, Customer $author): void
     {
         $topic->getId()->willReturn(1);
         $topic->getTitle()->willReturn("New message");
         $topic->getCreatedAt()->willReturn(new \DateTime());
+        $topic->getAuthor()->willReturn($author);
 
         $document = $this->create($topic);
         $document->shouldHaveType(AppDocument::class);
