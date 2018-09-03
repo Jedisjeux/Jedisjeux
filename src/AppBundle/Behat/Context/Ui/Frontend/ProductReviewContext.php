a355b9c32bdabb929e881d5f3a1b6f89037aa122
Loïc Frémont <lc.fremont@gmail.com>
2018-09-03 11:56:42 +0200

    add product review on todo feature

--- src/AppBundle/Behat/Context/Ui/Frontend/ProductReviewContext.php
+++ src/AppBundle/Behat/Context/Ui/Frontend/ProductReviewContext.php
@@ -2,24 +2,42 @@
 
 namespace AppBundle\Behat\Context\Ui\Frontend;
 
+use AppBundle\Behat\Page\Frontend\ProductReview\CreatePage;
 use AppBundle\Behat\Page\Frontend\ProductReview\UpdatePage;
 use AppBundle\Entity\ProductReview;
 use Behat\Behat\Context\Context;
+use Sylius\Component\Product\Model\ProductInterface;
 
 class ProductReviewContext implements Context
 {
+    /**
+     * @var CreatePage
+     */
+    private $createPage;
+
     /**
      * @var UpdatePage
      */
     private $updatePage;
 
     /**
-     * ProductReviewContext constructor.
+     * @param CreatePage $createPage
      * @param UpdatePage $updatePage
      */
-    public function __construct(UpdatePage $updatePage)
+    public function __construct(CreatePage $createPage, UpdatePage $updatePage)
     {
+        $this->createPage = $createPage;
         $this->updatePage = $updatePage;
+    }
+
+    /**
+     * @Given /^I want to review (this product)$/
+     */
+    public function iWantToReviewAProduct(ProductInterface $product)
+    {
+        $this->createPage->open([
+            'productId' => $product->getId(),
+        ]);
     }
 
     /**
@@ -47,6 +65,32 @@ class ProductReviewContext implements Context
     public function iChangeItsCommentAtAs($comment)
     {
         $this->updatePage->setComment($comment);
+    }
+
+    /**
+     * @When I leave a comment :comment, titled :title
+     */
+    public function iLeaveACommentTitled($comment = null, $title = null)
+    {
+        $this->createPage->setTitle($title);
+        $this->createPage->setComment($comment);
+    }
+
+    /**
+     * @When I rate it with :rate point(s)
+     */
+    public function iRateItWithPoints($rate)
+    {
+        $this->createPage->rateReview($rate);
+    }
+
+    /**
+     * @When I add it
+     * @When I try to add it
+     */
+    public function iAddIt()
+    {
+        $this->createPage->submit();
     }
 
     /**
