7a75154ed8cf9a4c02ba6076edf64a18d13b7333
Loïc Frémont <lc.fremont@gmail.com>
2018-10-04 18:11:07 +0200

    [WIP] remove cmf

--- src/AppBundle/DependencyInjection/Compiler/ServicesPass.php
+++ src/AppBundle/DependencyInjection/Compiler/ServicesPass.php
@@ -84,11 +84,6 @@ class ServicesPass implements CompilerPassInterface
         $contactRequestFactoryDefinition
             ->addMethodCall('setCustomerContext', [new Reference('app.context.customer')]);
 
-        $stringBlockFactoryDefinition = $container->getDefinition('app.factory.string_block');
-        $stringBlockFactoryDefinition
-            ->addArgument(new Reference('doctrine_phpcr.odm.document_manager'))
-            ->addArgument(new Parameter('cmf_block.persistence.phpcr.block_basepath'));
-
         $productListFactoryDefinition = $container->getDefinition('app.factory.product_list');
         $productListFactoryDefinition
             ->addArgument(new Reference('app.context.customer'))
