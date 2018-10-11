8c942a38c99e66fa108f16e4c9d4d6d256a1f160
Loïc Frémont <lc.fremont@gmail.com>
2018-10-11 15:28:30 +0200

    fix gulp file

--- Gulpfile.js
+++ Gulpfile.js
@@ -4,7 +4,7 @@ var argv = require('yargs').argv;
 
 config = [
     '--rootPath',
-    argv.rootPath || '../../../web/assets/',
+    argv.rootPath || '../../../public/assets/',
     '--nodeModulesPath',
     argv.nodeModulesPath || '../../../node_modules/',
     '--vendorPath',
@@ -12,25 +12,25 @@ config = [
 ];
 
 gulp.task('admin', function() {
-    gulp.src('web/assets/backend/Gulpfile.js', { read: false })
+    gulp.src('public/assets/backend/Gulpfile.js', { read: false })
         .pipe(chug({ args: config }))
     ;
 });
 
 gulp.task('admin-watch', function() {
-    gulp.src('web/assets/backend/Gulpfile.js', { read: false })
+    gulp.src('public/assets/backend/Gulpfile.js', { read: false })
         .pipe(chug({ args: config, tasks: 'watch' }))
     ;
 });
 
 gulp.task('app', function() {
-    gulp.src('web/assets/frontend/Gulpfile.js', { read: false })
+    gulp.src('public/assets/frontend/Gulpfile.js', { read: false })
         .pipe(chug({ args: config }))
     ;
 });
 
 gulp.task('app-watch', function() {
-    gulp.src('web/assets/frontend/Gulpfile.js', { read: false })
+    gulp.src('public/assets/frontend/Gulpfile.js', { read: false })
         .pipe(chug({ args: config, tasks: 'watch' }))
     ;
 });
