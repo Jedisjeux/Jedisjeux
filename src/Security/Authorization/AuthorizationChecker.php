0a6d5059aa9fb826df03390b3d4875dfa0344b4b
Loïc Frémont <lc.fremont@gmail.com>
2018-10-17 17:14:47 +0200

    fix some errors on log

---
+++ src/Security/Authorization/AuthorizationChecker.php
@@ -0,0 +1,60 @@
+<?php
+
+/*
+ * This file is part of Jedisjeux.
+ *
+ * (c) Loïc Frémont
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\Security\Authorization;
+
+use Symfony\Component\Security\Core\Authentication\AuthenticationManagerInterface;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+use Symfony\Component\Security\Core\Authorization\AccessDecisionManagerInterface;
+use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
+use Symfony\Component\Security\Core\Exception\AuthenticationCredentialsNotFoundException;
+
+/**
+ * Workaround for security
+ * In order to use isGranted on non authenticated user
+ */
+class AuthorizationChecker implements AuthorizationCheckerInterface
+{
+    private $tokenStorage;
+    private $accessDecisionManager;
+    private $authenticationManager;
+    private $alwaysAuthenticate;
+
+    public function __construct(TokenStorageInterface $tokenStorage, AuthenticationManagerInterface $authenticationManager, AccessDecisionManagerInterface $accessDecisionManager, bool $alwaysAuthenticate = false)
+    {
+        $this->tokenStorage = $tokenStorage;
+        $this->authenticationManager = $authenticationManager;
+        $this->accessDecisionManager = $accessDecisionManager;
+        $this->alwaysAuthenticate = $alwaysAuthenticate;
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @throws AuthenticationCredentialsNotFoundException when the token storage has no authentication token
+     */
+    final public function isGranted($attributes, $subject = null)
+    {
+        if (null === ($token = $this->tokenStorage->getToken())) {
+            return false;
+        }
+
+        if ($this->alwaysAuthenticate || !$token->isAuthenticated()) {
+            $this->tokenStorage->setToken($token = $this->authenticationManager->authenticate($token));
+        }
+
+        if (!\is_array($attributes)) {
+            $attributes = array($attributes);
+        }
+
+        return $this->accessDecisionManager->decide($token, $attributes, $subject);
+    }
+}
