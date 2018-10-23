9d05fa3fb16a9cbf46d7697d190dedcc1e455aaf
Loïc Frémont <lc.fremont@gmail.com>
2018-10-23 09:29:26 +0200

    handle product variant on product box type

--- src/Form/Type/ProductBoxType.php
+++ src/Form/Type/ProductBoxType.php
@@ -12,7 +12,11 @@
 namespace App\Form\Type;
 
 use App\Entity\ProductBox;
+use App\Entity\ProductVariant;
+use Doctrine\ORM\EntityRepository;
 use Sylius\Bundle\ProductBundle\Form\Type\ProductAutocompleteChoiceType;
+use Sylius\Bundle\ResourceBundle\Form\Type\ResourceAutocompleteChoiceType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -38,6 +42,27 @@ class ProductBoxType extends AbstractType
             ->add('realHeight', NumberType::class, [
                 'label' => 'app.ui.height_in_millimeter',
             ]);
+
+        /** @var ProductBox $productBox */
+        $productBox = $options['data'];
+
+        if (null !== $productBox && null !== $productBox->getProduct()) {
+            $builder
+                ->add('productVariant', EntityType::class, [
+                    'label' => 'sylius.ui.product_variants',
+                    'required' => false,
+                    'class' => ProductVariant::class,
+                    'choice_name' => 'name',
+                    'choice_value' => 'code',
+                    'query_builder' => function (EntityRepository $er) use ($productBox) {
+                        return $er->createQueryBuilder('o')
+                            ->where('o.product = :product')
+                            ->setParameter('product', $productBox->getProduct())
+                            ->orderBy('o.position');
+                    },
+                ])
+            ;
+        }
     }
 
     /**
