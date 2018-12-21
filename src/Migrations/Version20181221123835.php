235360758fe85433a1f5f17e95f8746cca9039ae
Loic Fremont <lc.fremont@gmail.com>
2018-12-21 13:39:36 +0100

    fix migrations

--- src/Migrations/Version20181221123835.php
+++ src/Migrations/Version20181221123835.php
@@ -8,14 +8,15 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20181221095521 extends AbstractMigration
+final class Version20181221123835 extends AbstractMigration
 {
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('CREATE TABLE jdj_year_award (id INT AUTO_INCREMENT NOT NULL, award_id INT DEFAULT NULL, year VARCHAR(255) NOT NULL, INDEX IDX_A28759703D5282CF (award_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE jdj_game_award (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, slug VARCHAR(255) DEFAULT NULL, UNIQUE INDEX UNIQ_33DA0E28989D9B62 (slug), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE jdj_year_award (id INT AUTO_INCREMENT NOT NULL, award_id INT DEFAULT NULL, year VARCHAR(255) NOT NULL, INDEX IDX_A28759703D5282CF (award_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
         $this->addSql('ALTER TABLE jdj_year_award ADD CONSTRAINT FK_A28759703D5282CF FOREIGN KEY (award_id) REFERENCES jdj_game_award (id)');
     }
 
@@ -24,6 +25,8 @@ final class Version20181221095521 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
+        $this->addSql('ALTER TABLE jdj_year_award DROP FOREIGN KEY FK_A28759703D5282CF');
+        $this->addSql('DROP TABLE jdj_game_award');
         $this->addSql('DROP TABLE jdj_year_award');
     }
 }
