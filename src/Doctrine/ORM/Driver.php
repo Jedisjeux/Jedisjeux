a18631b22d6bce44632b97b85e06d9c21f484db7
Loïc Frémont <lc.fremont@gmail.com>
2020-03-28 15:07:57 +0100

    Work around for grid bundle

---
+++ src/Doctrine/ORM/Driver.php
@@ -0,0 +1,61 @@
+<?php
+
+/*
+ * This file is part of mz_155_s_rebelote.
+ *
+ * (c) Mobizel
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace App\Doctrine\ORM;
+
+use Doctrine\Common\Persistence\ManagerRegistry;
+use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\ORM\EntityRepository;
+use Sylius\Component\Grid\Data\DataSourceInterface;
+use Sylius\Component\Grid\Data\DriverInterface;
+use Sylius\Component\Grid\Parameters;
+
+class Driver implements DriverInterface
+{
+    public const NAME = 'doctrine/orm';
+
+    /** @var ManagerRegistry */
+    private $managerRegistry;
+
+    public function __construct(ManagerRegistry $managerRegistry)
+    {
+        $this->managerRegistry = $managerRegistry;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getDataSource(array $configuration, Parameters $parameters): DataSourceInterface
+    {
+        if (!array_key_exists('class', $configuration)) {
+            throw new \InvalidArgumentException('"class" must be configured.');
+        }
+
+        /** @var ObjectManager $manager */
+        $manager = $this->managerRegistry->getManagerForClass($configuration['class']);
+
+        /** @var EntityRepository $repository */
+        $repository = $manager->getRepository($configuration['class']);
+
+        if (isset($configuration['repository']['method'])) {
+            $method = $configuration['repository']['method'];
+            $arguments = isset($configuration['repository']['arguments']) ? array_values($configuration['repository']['arguments']) : [];
+
+            $queryBuilder = $repository->$method(...$arguments);
+        } else {
+            $queryBuilder = $repository->createQueryBuilder('o');
+        }
+
+        return new DataSource($queryBuilder);
+    }
+}
